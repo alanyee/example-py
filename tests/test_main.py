442d6fa954b8b5d5afa755107f5131cff5b0af98
Alan Yee <alanyee@users.noreply.github.com>
2021-02-24 16:27:42 -0800

    Create test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+from example import main
+
+
+TEST_COW = """
+___________________
+< Moo! >
+ -------------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+"""
+
+def test_main():
+    assert TEST_COW == main.say()
