243df2d857ce3030138ba6dd1fe82da1377503cf
Alan Yee <alanyee@users.noreply.github.com>
2021-02-24 23:16:04 -0800

    Update main.py

--- example/main.py
+++ example/main.py
@@ -1,22 +1,30 @@
 # -*- coding: utf-8 -*-
 import sys
 
-COW = """
+COW = r"""
 ___________________
 < {} >
 -------------------
         \   ^__^
          \  (oo)\_______
-            (__)\       )\/\\
+            (__)\       )\/\
                 ||----w |
                 ||     ||
 """
 
-def say(cowsay):
+def say(cowsay: str) -> str:
+    """Interpolates what the cow says
+    Reference: https://sphinx-rtd-tutorial.readthedocs.io/en/latest/docstrings.html
+    :param cowsay: string for what the cow says
+    :type cowsay: str
+    
+    :return: cow ascii art interpolated with cowsay
+    :rtype: str
+    """
     return COW.format(cowsay)
  
-def main():
-    """Example"""
+def main() -> None:
+    """Print what the cow says"""
     cowsay = "Moo!"
     if len(sys.argv) > 1:
         cowsay = sys.argv[1]
